3e29093a1ff236ae37e92d03797f4600cf70f345
Jonathan Gerbaud <jonathan.gerbaud@gmail.com>
2019-06-10 15:28:25 +0200

    Added support for all drawable in MaskDelegate

--- ui-image/src/main/java/fr/jonathangerbaud/ui/image/MaskDelegate.kt
+++ ui-image/src/main/java/fr/jonathangerbaud/ui/image/MaskDelegate.kt
@@ -4,6 +4,12 @@ import android.graphics.*
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.view.View
+import android.R.attr.bitmap
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.graphics.Bitmap
+
+
 
 
 class MaskDelegate(view: View)
@@ -62,6 +68,18 @@ class MaskDelegate(view: View)
                 if (drawable is BitmapDrawable)
                 {
                     bitmap = (drawable as BitmapDrawable).bitmap
+                }
+                else
+                {
+                    if (drawable!!.intrinsicWidth <= 0 || drawable!!.intrinsicHeight <= 0)
+                        bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888) // Single color bitmap will be created of 1x1 pixel
+                    else
+                        bitmap = Bitmap.createBitmap(drawable!!.intrinsicWidth, drawable!!.intrinsicHeight, Bitmap.Config.ARGB_8888)
+
+                    val canvas = Canvas(bitmap!!)
+                    drawable!!.setBounds(0, 0, canvas.width, canvas.height)
+                    drawable!!.draw(canvas)
+                    return bitmap
                 }
             }
 
